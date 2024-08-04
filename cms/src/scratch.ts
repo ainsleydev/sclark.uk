fca0570e69aa6dd593377de7e1d0ce66469d759a
Ainsley <hello@ainsley.dev>
2024-08-04 10:46:53 +0100

    feat: Implementing picture/image providers for views

--- cms/src/scratch.ts
+++ cms/src/scratch.ts
@@ -1,15 +1,10 @@
-import { getPayloadConfig } from '@/seed/seed';
 import { htmlToLexical } from '@ainsleydev/payload-helper/dist/util/lexical';
-import { importWithoutClientFiles } from 'payload/node';
 
 const scratch = async () => {
-	const cfg = await importWithoutClientFiles('./payload.config.ts');
 	const str =
 		'<h2>Online English Lessons</h2><p>Are you worried about making mistakes in English? Do you know what to say in your head, but the words don’t come out? Can you speak some English, but it’s difficult to talk about complex topics? Do you need to pass an English exam?</p>';
-	// @ts-ignore
-	htmlToLexical(str, cfg).then((html) => {
-		console.log(JSON.stringify(html, null, 2));
-	});
+	const html = htmlToLexical(str)
+	console.log(JSON.stringify(html, null, 2));
 };
 
 scratch().catch((e) => console.error(e));
