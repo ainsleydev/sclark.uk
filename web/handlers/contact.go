0493867a98138795b12b831483964614c3efc20a
Ainsley <hello@ainsley.dev>
2024-08-05 18:46:44 +0100

    refactor: Styling tweaks

--- web/handlers/contact.go
+++ web/handlers/contact.go
@@ -1,10 +1,19 @@
 package handlers
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"log/slog"
+	"net/http"
+
 	"github.com/go-ozzo/ozzo-validation"
 	"github.com/go-ozzo/ozzo-validation/is"
 
 	"github.com/ainsleyclark/go-payloadcms"
+	"github.com/ainsleydev/sclark.uk/views/components"
+	"github.com/ainsleydev/webkit/pkg/adapters/payload"
+	"github.com/ainsleydev/webkit/pkg/util/httputil"
 
 	"github.com/ainsleydev/webkit/pkg/webkit"
 )
@@ -35,76 +44,86 @@ func (c ContactForm) Validate() error {
 }
 
 // Contact is the handler for when a form has been submitted.
-func Contact(_ *payloadcms.Client, _ string) webkit.Handler {
+func Contact(_ *payloadcms.Client, web3FormsKey string) webkit.Handler {
 	return func(c *webkit.Context) error {
+		slog.Info("Received contact form request")
+
 		if err := c.Request.ParseForm(); err != nil {
 			return err
 		}
 
-		_ = ContactForm{
+		form := ContactForm{
 			Name:    c.Request.FormValue("name"),
 			Email:   c.Request.FormValue("email"),
 			Message: c.Request.FormValue("message"),
 		}
 
-		return nil
+		var formHTML payload.Form
+		err := json.Unmarshal([]byte(c.Request.FormValue("payload-form")), &formHTML)
+		if err != nil {
+			slog.Error("Obtaining Payload Form")
+			return err
+		}
 
-		//formHTML := content.ContactForm.Form
-		//for idx, field := range formHTML.Fields {
-		//	if field.Name == "name" {
-		//		formHTML.Fields[idx].Value = form.Name
-		//	}
-		//	if field.Name == "email" {
-		//		formHTML.Fields[idx].Value = form.Email
-		//	}
-		//	if field.Name == "message" {
-		//		formHTML.Fields[idx].Value = form.Message
-		//	}
-		//}
-		//
-		//if err := form.Validate(); err != nil {
-		//	var e validation.Errors
-		//	if !errors.As(err, &e) {
-		//		slog.Error("Failed to validate contact form: %v", err)
-		//	}
-		//	return c.Render(components.Form(components.FormProps{
-		//		Form:   formHTML,
-		//		Errors: e,
-		//	}))
-		//}
-		//
-		//web3Form := Web3FormsRequest{
-		//	ContactForm: form,
-		//	APIKey:      web3FormsKey,
-		//	Subject:     "S.Clark - New contact form submission from: " + form.Name,
-		//}
-		//
-		//f := components.Form(components.FormProps{
-		//	Form:    formHTML,
-		//	Errors:  nil,
-		//	Success: false,
-		//})
-		//
-		//buf, err := json.Marshal(web3Form)
-		//if err != nil {
-		//	slog.Error("Failed to marshal Web3Forms request: %v", err)
-		//	return c.Render(f)
-		//}
-		//
-		//post, err := http.Post("https://api.web3forms.com/submit", "application/json", bytes.NewBuffer(buf))
-		//if err != nil {
-		//	slog.Error("Failed to post contact form: " + err.Error())
-		//	return c.Render(f)
-		//}
-		//
-		//if !httputil.Is2xx(post.StatusCode) {
-		//	slog.Error("Contact form came back with a bad status", "stats", post.StatusCode)
-		//	return c.Render(f)
-		//}
-		//
-		//return c.Render(components.Form(components.FormProps{
-		//	Form:    formHTML,
-		//	Success: true,
-		//}))
+		for idx, field := range formHTML.Fields {
+			if field.Name == "name" {
+				formHTML.Fields[idx].Value = form.Name
+			}
+			if field.Name == "email" {
+				formHTML.Fields[idx].Value = form.Email
+			}
+			if field.Name == "message" {
+				formHTML.Fields[idx].Value = form.Message
+			}
+		}
+
+		if err := form.Validate(); err != nil {
+			var e validation.Errors
+			if !errors.As(err, &e) {
+				slog.Error("Failed to validate contact form: %v", err)
+			}
+			return c.Render(components.Form(components.FormProps{
+				Form:   formHTML,
+				Errors: e,
+			}))
+		}
+
+		web3Form := Web3FormsRequest{
+			ContactForm: form,
+			APIKey:      web3FormsKey,
+			Subject:     "S.Clark - New contact form submission from: " + form.Name,
+		}
+
+		f := components.Form(components.FormProps{
+			Form:    formHTML,
+			Errors:  nil,
+			Success: false,
+		})
+
+		buf, err := json.Marshal(web3Form)
+		if err != nil {
+			slog.Error("Failed to marshal Web3Forms request: %v", err)
+			return c.Render(f)
+		}
+
+		slog.Info("Preparing to submit form request to Web 3 Forms")
+
+		post, err := http.Post("https://api.web3forms.com/submit", "application/json", bytes.NewBuffer(buf))
+		if err != nil {
+			slog.Error("Failed to post contact form: " + err.Error())
+			return c.Render(f)
+		}
+
+		if !httputil.Is2xx(post.StatusCode) {
+			slog.Error("Contact form came back with a bad status", "stats", post.StatusCode)
+			return c.Render(f)
+		}
+
+		slog.Info("Contact form email sent successfully")
+
+		return c.Render(components.Form(components.FormProps{
+			Form:    formHTML,
+			Success: true,
+		}))
 	}
 }
