331f9ead0b1db3f415cd53b9b06c096e384c21b9
Ainsley <hello@ainsley.dev>
2024-08-05 20:25:22 +0100

    test: Trying to fix libsql error

--- infra/db.tf
+++ infra/db.tf
@@ -1,37 +1,37 @@
 # Database
 
-# resource "null_resource" "turso_database" {
-# 	triggers = {
-# 		db_name = var.turso_config.db_name
-# 	}
-#
-# 	provisioner "local-exec" {
-# 		when = create
-# 		command = "turso db create ${self.triggers.db_name}"
-# 	}
-#
-# 	provisioner "local-exec" {
-# 		when    = destroy
-# 		command = "turso db destroy ${self.triggers.db_name} --yes"
-# 		on_failure = continue
-# 	}
-# }
-#
-# resource "null_resource" "turso_token" {
-# 	depends_on = [null_resource.turso_database]
-#
-# 	provisioner "local-exec" {
-# 		when = create
-# 		command = "turso db tokens create ${var.turso_config.db_name} > db-token.txt --expiration never"
-# 	}
-# }
-#
-# data "local_file" "token" {
-# 	depends_on = [null_resource.turso_token]
-# 	filename   = "db-token.txt"
-# }
-#
-# output "turso_token" {
-# 	value     = trimspace(data.local_file.token.content)
-# 	sensitive = true
-# }
+resource "null_resource" "turso_database" {
+	triggers = {
+		db_name = var.turso_config.db_name
+	}
+
+	provisioner "local-exec" {
+		when = create
+		command = "turso db create ${self.triggers.db_name}"
+	}
+
+	provisioner "local-exec" {
+		when    = destroy
+		command = "turso db destroy ${self.triggers.db_name} --yes"
+		on_failure = continue
+	}
+}
+
+resource "null_resource" "turso_token" {
+	depends_on = [null_resource.turso_database]
+
+	provisioner "local-exec" {
+		when = create
+		command = "turso db tokens create ${var.turso_config.db_name} > db-token.txt --expiration never"
+	}
+}
+
+data "local_file" "token" {
+	depends_on = [null_resource.turso_token]
+	filename   = "db-token.txt"
+}
+
+output "turso_token" {
+	value     = trimspace(data.local_file.token.content)
+	sensitive = true
+}
