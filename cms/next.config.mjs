e34c2b3576278acde1abd6079ebc25b3836c5741
Ainsley <hello@ainsley.dev>
2024-08-08 16:48:01 +0100

    platform: Increasing http timeout

--- cms/next.config.mjs
+++ cms/next.config.mjs
@@ -3,6 +3,9 @@ import { withPayload } from '@payloadcms/next/withPayload';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'standalone',
+	serverRuntimeConfig: {
+		timeout: 60,
+	},
 	redirects: async () => {
 		return [
 			{
