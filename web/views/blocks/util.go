177a9dd0932787187284b20c3091d1e2b41c41e4
Ainsley <hello@ainsley.dev>
2024-08-08 12:09:21 +0100

    fix: Identifiers

--- web/views/blocks/util.go
+++ web/views/blocks/util.go
@@ -2,9 +2,9 @@ package blocks
 
 import "github.com/ainsleydev/webkit/pkg/util/random"
 
-func getID(id *string) string {
-	if id == nil {
-		return random.Alpha(16)
+func getID(id string) string {
+	if id == "" {
+		return random.Seq(16)
 	}
-	return *id
+	return id
 }
